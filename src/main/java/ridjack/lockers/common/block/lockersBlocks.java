b734622738344197cab805f2ce1bd48ddbc1d791
ridjeck <josh@ktpstudios.com>
2020-04-28 19:12:22 -0400

    New Locker Textures!

--- src/main/java/ridjack/lockers/common/block/lockersBlocks.java
+++ src/main/java/ridjack/lockers/common/block/lockersBlocks.java
@@ -6,10 +6,10 @@ import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
-public final class lockersBlocks
-{
-	public static final DeferredRegister< Block >			REGISTERY			= new DeferredRegister<>( ForgeRegistries.BLOCKS, ModInfo.MOD_ID );
+	public final class lockersBlocks
+	{
+		public static final DeferredRegister< Block >			REGISTERY			= new DeferredRegister<>( ForgeRegistries.BLOCKS, ModInfo.MOD_ID );
 
 
-	public static RegistryObject< BlockLocker >				LOCKER				= REGISTERY.register( "locker", () -> new BlockLocker() );
-}
+		public static RegistryObject< BlockLocker >				LOCKER				= REGISTERY.register( "locker", () -> new BlockLocker() );
+	}
